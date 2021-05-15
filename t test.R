8621c431b0d7c99a9e90d6d339a03a9ee7432e71
Conor Carroll <45122826+CC-94@users.noreply.github.com>
2021-05-15 18:21:42 +0100

    Add files via upload

--- t test.R
+++ t test.R
@@ -1,4 +1,19 @@
-library(dplyr)
-str(Final_Data)
+shapiro.test(Data$Male)
+shapiro.test(Data$Female)
 
-table(Final_Data$Genre, Final_Data$Gender)
+hist(Data$Male,
+     main="Brand Mentions by Male Artists",
+     xlab="Number of mentions",
+     xlim=c(0,250),
+     col="darkmagenta",
+     freq=FALSE
+)
+hist(Data$Female,
+     main="Brand Mentions by Female Artists",
+     xlab="Number of mentions",
+     xlim=c(0,140),
+     col="darkblue",
+     freq=FALSE
+)
+
+t.test(Data$Male, Data$Female)
